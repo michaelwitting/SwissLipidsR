d0616944d83775c5fe08d0ce058955ec85f8e0f1
Michael Witting <michael.witting@helmholtz-muenchen.de>
2020-04-01 18:14:17 +0200

    added unit tests for mapping

---
+++ tests/testthat/test_mapping.R
@@ -0,0 +1,16 @@
+test_that("correct calculation of adduct m/z", {
+
+  from <- "SwissLipids"
+  to <- "LipidMaps"
+  ids <- c("SLM:000048885", "SLM:000000651")
+
+  results <- swissLipidsMapping(from, to, ids)
+
+  # test correct length of list
+  expect_equal(length(results), 2)
+
+  # check for correct ids
+  expect_equal(results$to[[1]]$id, "LMGP01030010")
+  expect_equal(results$to[[2]]$id, "LMGP01010005")
+
+})
