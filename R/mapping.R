27a303a6bd0632ebff47b86dcafdc398c7321ca8
Michael Witting <michael.witting@helmholtz-muenchen.de>
2020-04-01 18:14:04 +0200

    added documentation for mapping

--- R/mapping.R
+++ R/mapping.R
@@ -1,3 +1,30 @@
+#' @title SwissLipids API Mapping
+#'
+#' @description
+#'
+#' `swissLipidsMapping` uses the SwissLipids API to perform mapping between
+#'     different database identifier
+#'
+#' @param from `character` Abbrevation of data from which the ids shall be mapped
+#' @param to `character` Abbrevation of database to which the ids shall be mapped
+#' @param ids `character` A vector of IDs that shall be mapped.
+#'
+#' @return `list` A named list of the same length as ids is returned.
+#'
+#' @author Michael Witting
+#'
+#'
+#' @importFrom jsonlite fromJSON
+#'
+#' @export
+#'
+#' @examples
+#'
+#' ids <- c("SLM:000048885", "SLM:000000651")
+#' from <- "SwissLipids"
+#' to <- "LipidMaps"
+#'
+#' swissLipidsMapping(from, to, ids)
 swissLipidsMapping <- function(from = c("SwissLipids", "LipidMaps", "ChEBI", "HMDB"),
                                to = c("SwissLipids", "LipidMaps", "ChEBI", "HMDB", "UniProtKB", "Rhea"),
                                ids) {
